21892363a50bd5dd5bfc803db3cf8918502ff892
leonw774 <leonw774@gmail.com>
2024-02-28 13:09:43 +0800

    try font fallback 2

--- _site/main.js
+++ _site/main.js
@@ -110,9 +110,9 @@ function importRuffleRS() {
             "/assets/font-swfs/homestuck-fonts.swf",
         ];
         defaultFonts = {
-            "sans": ["Noto Sans CJK TC Regular", "Verdana"],
-            "serif": ["Noto Serif CJK TC Regular", "Times New Roman"],
-            "typewriter": ["Noto Sans CJK TC Bold", "Courier New"]
+            "sans": ["Verdana", "Noto Sans CJK TC Regular"],
+            "serif": ["Times New Roman", "Noto Serif CJK TC Regular"],
+            "typewriter": ["Courier New", "Noto Sans CJK TC Bold"]
         };
     }
     
